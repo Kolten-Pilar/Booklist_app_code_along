75ef6da2da6c08fc501cad7560454010d20db8f1
Kolten ✔️ <kolten.pilar@gmail.com>
2023-06-21 20:46:24 -0400

    save

--- app.js
+++ app.js
@@ -42,7 +42,13 @@ class UI {
 
     list.appendChild(row);
   }
+  static clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#isbn').value = '';
+  }
 }
+
 
 // Store Class: Handles Storage
 
@@ -50,5 +56,23 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  // Prevent actual submit
+  e.preventDefault();
+  
+  // get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+
+  // instatiate book
+  const book = new Book(title, author, isbn);
+
+  // add book to UI
+  UI.addBookToList(book);
+
+  // Clear fields
+  UI.clearFields();
+});
 
 // Event: Remove a Book
